f6a1cf68945c0df630b86ef464f000b5fbff87f4
Noopur Jain <83579850+jainoopur@users.noreply.github.com>
2023-02-15 11:14:21 +0800

    works for till 59

--- roman_to_arabic.py
+++ roman_to_arabic.py
@@ -1,20 +1,24 @@
+ARABIC_VALUE = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50
+}
+
+
 def roman_to_arabic(roman):
     count = 0
     pos = 0
     for char in roman:
-        if char == 'I':
-            if pos + 1 < len(roman) and roman[pos + 1] != 'I':
-                count = count - 1
+        value = ARABIC_VALUE[char]
+        if pos + 1 < len(roman) and (char == 'I' or char == 'X'):
+            next_char = roman[pos + 1]
+            next_char_value = ARABIC_VALUE[next_char]
+            if value < next_char_value:
+                count = count - value
             else:
-                count = count + 1
-        elif char == 'V':
-            count = count + 5
-        elif char == 'X':
-            if pos + 1 < len(roman) and roman[pos + 1] != 'X' or roman[pos + 1]:
-                count = count - 10
-            count = count + 10
-        elif char == 'L':
-            count = count + 50
+                count = count + value
+        else:
+            count = count + value
         pos = pos + 1
     return count
-
