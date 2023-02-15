d6697b452cf666276b3e15456e3fcf0ffd857b9d
Noopur Jain <83579850+jainoopur@users.noreply.github.com>
2023-02-15 10:50:38 +0800

    Works for 4 but not 9

---
+++ roman_to_arabic.py
@@ -0,0 +1,16 @@
+def roman_to_arabic(roman):
+    count = 0
+    pos = 0
+    for char in roman:
+        if char == 'I':
+            if pos + 1 < len(roman) and roman[pos + 1] == 'V':
+                count = count - 1
+            else:
+                count = count + 1
+        elif char == 'V':
+            count = count + 5
+        elif char == 'X':
+            count = count + 10
+        pos = pos + 1
+    return count
+
---
+++ test_roman_to_arabic.py
@@ -0,0 +1,28 @@
+import unittest
+
+from roman_to_arabic import roman_to_arabic
+
+
+# 1, 2, 5, 6
+class TestRomanToArabic(unittest.TestCase):
+    def test_I(self):
+        self.assertEqual(1, roman_to_arabic("I"))
+
+    def test_II(self):
+        self.assertEqual(2, roman_to_arabic("II"))
+
+    # Do we need this? It is not a failing test...
+    def test_III(self):
+        self.assertEqual(3, roman_to_arabic("III"))
+
+    def test_V(self):
+        self.assertEqual(5, roman_to_arabic("V"))
+
+    def test_VI(self):
+        self.assertEqual(6, roman_to_arabic("VI"))
+
+    def test_IV(self):
+        self.assertEqual(4, roman_to_arabic("IV"))
+
+    def test_X(self):
+        self.assertEqual(10, roman_to_arabic("X"))
