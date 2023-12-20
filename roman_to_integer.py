485ee474f61854a61e62faeae3ae4bcfbd0e257d
Noopur Jain <83579850+jainoopur@users.noreply.github.com>
2023-12-20 15:31:49 +0800

    remove blank space

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -4,7 +4,6 @@ roman_integer_map = {
     'X': 10
 }
 
-
 def roman_to_integer(roman):
     result = 0
 
@@ -15,7 +14,6 @@ def roman_to_integer(roman):
         else:
             if index < len(roman) - 1 and (roman[index + 1] == 'V' or roman[index + 1] == 'X'):
                 result = result - roman_integer_map[character]
-
             else:
                 result = result + roman_integer_map[character]
 
